00c803d1939d7a8fb4819f03390c1dded22982e3
shaikot07 <saifulislamshaikat007@gmail.com>
2023-12-22 02:15:25 +0600

    added deriver and connect database

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -9,6 +11,47 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
+// pass: HGjVqiLDfN8PWQhJ
+// user: task-managemnt
+console.log(process.env.DB_PASS);
+console.log(process.env.DB_USER);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.loifkbc.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = "mongodb+srv://<username>:<password>@cluster0.loifkbc.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+
+
+      const taskCollection =client.db('taskDb').collection('task')
+
+      app.post('/tasks', async(req,res)=>{
+            const task =req.body;
+            const result =await taskCollection.insertOne(task);
+            res.send(result)
+      })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+//     await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req,res)=>{
